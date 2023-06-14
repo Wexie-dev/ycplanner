64da6e383da955d0726e480e612db82b50bc3444
Rocco <roccotc@gmail.com>
2023-06-13 23:56:48 -0300

    feat: componente Maintenance

---
+++ src/pages/maintenance.tsx
@@ -0,0 +1,32 @@
+import React from "react"
+const logo = "/assets/images/ycplanner_logo.png"
+
+function maintenance() {
+  return (
+    <section
+      id="Construction"
+      className="flex justify-center items-center w-screen h-screen bg-[#F5F5F5]"
+    >
+      <div className="flex items-center flex-col w-[360px] md:w-[517px] lg:w-[620px] xl:w-[744px] h-[595px] md:h-[448px] lg:h-[537px] xl:h-[537px] bg-white border-2 rounded-[10px] shadow-[0px_4px_4px_rgba(0,0,0,0.25)]">
+        <div className="w-[199px] md:w-[127px] lg:w-[159px] mt-[62px] md:mt-[51px] lg:mt-16 xl:mt-20">
+          <img src={logo}></img>
+        </div>
+        <div className="mt-[36px] md:mt-3 lg:mt-4 xl:mt-5 font-normal text-2xl md:text-4 lg:text-5 text-[#4D4C4C]">
+          ¡Próximamente!
+        </div>
+        <div className="md:w-[355px] lg:w-[444px] xl:w-[555px] mt-10 md:mt-3 lg:mt-4 xl:mt-5 text-base text-center text-[#4D4C4C]">
+          Estoy construyendo mi web para que puedas conocer más sobre mí y cómo
+          puedo ayudarte a planificar el día más especial de tu vida.
+        </div>
+        <div className="mt-7 md:mt-[18px] lg:mt-[22px] text-base text-center text-[#4D4C4C]">
+          Mientras tanto 👇
+        </div>
+        <div className="flex justify-center items-center mt-7 md:mt-[18px] lg:mt-[22px] w-[241px] h-[45px] bg-[#C2857D] text-white rounded-[10px]">
+          Contactame en Instagram
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default maintenance
