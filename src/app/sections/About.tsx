52f3b59b488ea368ddb39b750dc786996a75e1ee
Rocco <roccotc@gmail.com>
2023-07-24 23:23:07 -0300

    feat: arreglos en los componentes

--- src/app/sections/About.tsx
+++ src/app/sections/About.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 export default function About() {
   return (
-    <section className="h-screen" id="about">
-      About
+    <section className="h-screen bg-slate-500 pt-20" id="about">
+      <div>About</div>
     </section>
   );
 }
