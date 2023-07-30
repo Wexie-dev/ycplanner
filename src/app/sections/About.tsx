3a0f5b29dec8e3f098846e4b9d402afc1ad51304
Jose Luis Raffalli <raffallijoseluis@gmail.com>
2023-07-30 13:33:50 -0300

    feat:  theme colors

--- src/app/sections/About.tsx
+++ src/app/sections/About.tsx
@@ -3,8 +3,8 @@ import React from 'react';
 
 export default function About(aboutProps: AboutProps) {
   return (
-    <section className="h-screen bg-slate-500 pt-20" id="about">
-      <div>About</div>
+    <section className="pt-20" id="about">
+      <h1></h1>
     </section>
   );
 }
