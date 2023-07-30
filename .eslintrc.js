05f898a96c9346b38c26db5c535ca943518ff1d1
Jose Luis Raffalli <raffallijoseluis@gmail.com>
2023-07-30 12:20:01 -0300

    feat: eslint unused imports rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,14 @@
 module.exports = {
-  extends: ["next", "next/core-web-vitals", "plugin:prettier/recommended"],
+  extends: [
+    'next',
+    'next/core-web-vitals',
+    'plugin:prettier/recommended',
+    'plugin:unused-imports/recommended',
+  ],
+  plugins: ["prettier", "unused-imports"],
   rules: {
     // Add the rule to enforce the same line endings as Prettier
-    "prettier/prettier": ["error", { endOfLine: "auto" }],
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+    "unused-imports/no-unused-imports": "error"
   },
 };
