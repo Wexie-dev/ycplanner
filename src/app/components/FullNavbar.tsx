dd8c33d3e0ec03a0bc4c0ae8efec99ac7994e735
Juan Antonio Di Cecco <juandicecco@gmail.com>
2023-08-02 20:39:06 -0400

    fix: Gallery

--- src/app/components/FullNavbar.tsx
+++ src/app/components/FullNavbar.tsx
@@ -8,9 +8,9 @@ function FullNavbar() {
   const matchedLG = useMediaQuery(mediaQueries.tablet);
 
   return (
-    <nav className="duration-50 left-0 top-0 z-50 w-full bg-white shadow-lg ease-in sm:fixed md:h-[72px] 2xl:h-[80px]">
-      <div className="container px-4 md:ml-0 2xl:ml-[136px]">
-        <div className="flex w-full items-center justify-between py-3">
+    <nav className="duration-50 left-0 top-0 z-50 flex w-full items-center bg-white shadow-lg ease-in sm:fixed md:h-[72px] 2xl:h-[80px]">
+      <div className="container mr-0 px-4 md:ml-0 2xl:ml-[152px]">
+        <div className="flex w-full items-center justify-between">
           <div className="flex items-center">
             <Image width={matchedLG ? 48 : 72} height={matchedLG ? 40 : 64} src={logo} alt="Logo" />
             <div className="flex w-[350px] font-butler font-normal text-[#36373A] lg:text-[14px] 2xl:text-[20px]">
@@ -18,7 +18,7 @@ function FullNavbar() {
             </div>
           </div>
           {/* Add your navigation links here */}
-          <ul className="hidden-[100px] font-montserrat w-full justify-end gap-8 text-[#5B5A5A] lg:flex lg:space-x-3 lg:text-[16px] 2xl:space-x-8 2xl:text-[18px] 2xl:font-semibold">
+          <ul className="hidden-[100px] font-montserrat mr-0 w-full justify-start gap-8 text-[#5B5A5A] md:justify-end lg:flex lg:space-x-3 lg:text-[16px] 2xl:mr-[22px] 2xl:space-x-8 2xl:text-[18px] 2xl:font-semibold">
             <li className="transition-all duration-300 ease-in-out">
               <SmoothScrollLink to="#about">Sobre mi</SmoothScrollLink>
             </li>
@@ -31,7 +31,7 @@ function FullNavbar() {
             <li className="transition-all duration-300 ease-in-out">
               <SmoothScrollLink to="#">Testimonios</SmoothScrollLink>
             </li>
-            <li className="transition-all duration-300 ease-in-out">
+            <li className="transition-all duration-300 ease-in-out 2xl:mr-[88px]">
               <SmoothScrollLink to="#">Contactame</SmoothScrollLink>
             </li>
           </ul>
--- src/app/sections/Gallery.tsx
+++ src/app/sections/Gallery.tsx
@@ -10,60 +10,60 @@ function Gallery(galleryProps: GalleryProps) {
 
   return (
     <section id="gallery" className="h-screen w-full pt-20">
-      <div className="mb-[24px] mt-[24px] flex justify-center lg:mb-[32px] lg:mt-[32px] 2xl:mb-[56px] 2xl:mt-[56px]">
-        <div className="font-butler text-[24px] font-normal text-[#36373A] lg:text-[26px] 2xl:text-5xl">
+      <div className="mb-[24px] mt-[24px] flex justify-center sm:mb-[32px] sm:mt-[32px] 2xl:mb-[56px] 2xl:mt-[56px]">
+        <div className="font-butler text-[24px] font-normal text-[#36373A] sm:text-[26px] 2xl:text-5xl">
           {title}
         </div>
       </div>
       <div className="h-full w-full">
-        <div className="mx-[10px] h-[1592px] lg:mx-[24px] lg:h-[424px] 2xl:mx-[72px] 2xl:h-[776px]">
-          <div className="flex h-full w-full flex-col justify-center gap-[8px] sm:flex-row lg:gap-[8px] 2xl:gap-[16px]">
-            <div className="mx-auto flex-col lg:mx-0">
+        <div className="mx-[10px] h-[1592px] sm:mx-[24px] sm:h-[424px] 2xl:mx-[72px] 2xl:h-[776px]">
+          <div className="flex h-full w-full flex-col justify-center gap-[8px] sm:flex-row sm:gap-[8px] 2xl:gap-[16px]">
+            <div className="mx-auto flex-col sm:mx-0">
               <img
                 src={isMobile ? mobileImages[0].url : images[0].url}
                 alt="Imagen 1"
-                className="mb-[8px] h-[192px] w-[328px] bg-slate-500 object-cover lg:mb-[8px] lg:h-[176px] lg:w-[237px] 2xl:mb-[16px] 2xl:h-[320px] 2xl:w-[432px]"
+                className="mb-[8px] h-[192px] w-[328px] bg-slate-500 object-cover sm:mb-[8px] sm:h-[176px] sm:w-[237px] 2xl:mb-[16px] 2xl:h-[320px] 2xl:w-[432px]"
               />
               <img
                 src={isMobile ? mobileImages[1].url : images[1].url}
                 alt="Imagen 2"
-                className="h-[192px] w-[328px] bg-slate-500 object-cover lg:h-[240px] lg:w-[237px] 2xl:h-[440px] 2xl:w-[432px]"
+                className="h-[192px] w-[328px] bg-slate-500 object-cover sm:h-[240px] sm:w-[237px] 2xl:h-[440px] 2xl:w-[432px]"
               />
             </div>
-            <div className="mx-auto flex-col lg:mx-0">
+            <div className="mx-auto flex-col sm:mx-0">
               <img
                 src={isMobile ? mobileImages[2].url : images[2].url}
                 alt="Imagen 3"
-                className="mb-[8px] h-[192px] w-[328px] bg-slate-500 object-cover lg:mb-[8px] lg:h-[248px] lg:w-[240px] 2xl:mb-[16px] 2xl:h-[448px] 2xl:w-[432px]"
+                className="mb-[8px] h-[192px] w-[328px] bg-slate-500 object-cover sm:mb-[8px] sm:h-[248px] sm:w-[240px] 2xl:mb-[16px] 2xl:h-[448px] 2xl:w-[432px]"
               />
               <img
                 src={isMobile ? mobileImages[3].url : images[3].url}
                 alt="Imagen 4"
-                className="h-[192px] w-[328px] bg-slate-500 object-cover lg:h-[168px] lg:w-[240px] 2xl:h-[312px] 2xl:w-[432px]"
+                className="h-[192px] w-[328px] bg-slate-500 object-cover sm:h-[168px] sm:w-[240px] 2xl:h-[312px] 2xl:w-[432px]"
               />
             </div>
-            <div className="mx-auto flex-col lg:mx-0">
+            <div className="mx-auto flex-col sm:mx-0">
               <img
                 src={isMobile ? mobileImages[4].url : images[4].url}
                 alt="Imagen 5"
-                className="mb-[8px] h-[192px] w-[328px] bg-slate-500 object-cover lg:mb-[8px] lg:h-[208px] lg:w-[240px] 2xl:mb-[16px] 2xl:h-[380px] 2xl:w-[432px]"
+                className="mb-[8px] h-[192px] w-[328px] bg-slate-500 object-cover sm:mb-[8px] sm:h-[208px] sm:w-[240px] 2xl:mb-[16px] 2xl:h-[380px] 2xl:w-[432px]"
               />
               <img
                 src={isMobile ? mobileImages[5].url : images[5].url}
                 alt="Imagen 6"
-                className="h-[192px] w-[328px] bg-slate-500 object-cover lg:h-[208px] lg:w-[240px] 2xl:h-[380px] 2xl:w-[432px]"
+                className="h-[192px] w-[328px] bg-slate-500 object-cover sm:h-[208px] sm:w-[240px] 2xl:h-[380px] 2xl:w-[432px]"
               />
             </div>
-            <div className="mx-auto flex-col lg:mx-0">
+            <div className="mx-auto flex-col sm:mx-0">
               <img
                 src={isMobile ? mobileImages[6].url : images[6].url}
                 alt="Imagen 7"
-                className="mb-[8px] h-[192px] w-[328px] bg-slate-500 object-cover lg:mb-[8px] lg:h-[280px] lg:w-[235px] 2xl:mb-[16px] 2xl:h-[512px] 2xl:w-[432px]"
+                className="mb-[8px] h-[192px] w-[328px] bg-slate-500 object-cover sm:mb-[8px] sm:h-[280px] sm:w-[235px] 2xl:mb-[16px] 2xl:h-[512px] 2xl:w-[432px]"
               />
               <img
                 src={isMobile ? mobileImages[7].url : images[7].url}
                 alt="Imagen 8"
-                className="h-[192px] w-[328px] bg-slate-500 object-cover lg:h-[136px] lg:w-[235px] 2xl:h-[248px] 2xl:w-[432px]"
+                className="h-[192px] w-[328px] bg-slate-500 object-cover sm:h-[136px] sm:w-[235px] 2xl:h-[248px] 2xl:w-[432px]"
               />
             </div>
           </div>
